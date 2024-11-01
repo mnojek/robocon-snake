f220afd4cc41ef31ee070da965b0b59c15d6da86
Mateusz Nojek <matnojek@gmail.com>
2024-11-01 14:21:41 +0100

    Add TODOs

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,10 @@
 // TODO: Add bigger maps
 // TODO: Save highest score of the player
 // TODO: Extract testReport to separate file
+// TODO: Bigger margins for the game canvas
+// TODO: The canvas moves a bit when the game is over
+// TODO: Preload canvas size to avoid strange behavior when game starts
+// TODO: Display 3 top scores a bit bigger
 
 import { defaultGameSettings } from "./defaultGameSettings.js";
 import {
